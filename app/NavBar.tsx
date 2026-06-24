b6ce5209bdeb9d20847d7b1ca41bab48b9d129a5
gelila-tilahun <gelila.t2020@gmail.com>
2026-06-24 12:02:36 +0300

    Refactor NavBar component and add react-icons dependency

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,23 @@
 import Link from 'next/link'
 import React from 'react'
+/* import {FaBugs}from 'react-icons/fa';*/
+
+
 
 const NavBar = () => {
+  const links = [
+    {label: 'Dashboard',  href: '/'},
+    {label: 'Issues', href: '/issues'}
+  ]
   return (
-    <nav>  
-      <Link href="/">Logo </Link>
-      <ul>
-         <li> <Link href="/"> Dashboard </Link></li>
-         <li> <Link href="/issues"> Issues </Link> </li>
+    <nav className='flex space-x-6 border-b-2 mb-5 px-5 h-14 items-center'>  
+      <Link href="/"> Logo </Link> 
+      <ul className='flex space-x-6'>
+        {links.map(link =>
+            <Link 
+              key={link.href} 
+              className= 'text-zinc-500 hover:text-zinc-800 transition-colors' 
+              href={link.href}> {link.label} </Link>)}  
       </ul>
     </nav>
 )
--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body className="{inter.className}">
         <NavBar />
         <main>  {children} </main>
-        {children}
+       
         </body>
     </html>
   );
