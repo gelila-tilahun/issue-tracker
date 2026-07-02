0d4f0ce6e51b87a367b66f3b9120d76bf83fd7d3
gelila-tilahun <gelila.t2020@gmail.com>
2026-07-02 13:33:42 +0300

    Refactor:Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -22,8 +22,21 @@ const NewIssuePage = () => {
   const {register, control, handleSubmit, formState: { errors } } =  useForm<IssueForm>({
     resolver: zodResolver(createIssueSchema)
   });
+
   const [error, setErrorMessage] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
+ 
+  const onSubmit = handleSubmit(async (data) => {
+          try {
+            setIsSubmitting(true);
+            await axios.post('/api/issues', data);
+            router.push('/issues');
+          } catch (error) {
+            setIsSubmitting(false);
+            setErrorMessage('An unexpected error occurred.');
+          }
+        });
+
 return (
     <div className='max-w-xl '>
       {error && (
@@ -34,16 +47,7 @@ return (
 
       <form
         className='space-y-3'
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setIsSubmitting(true);
-            await axios.post('/api/issues', data);
-            router.push('/issues');
-          } catch (error) {
-            setIsSubmitting(false);
-            setErrorMessage('An unexpected error occurred.');
-          }
-        })}
+        onSubmit={onSubmit}
       >
 
       <TextField.Root 
