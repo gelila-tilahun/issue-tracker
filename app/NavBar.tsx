66cb7098747987e9fa03715d88c2c1e38b13e729
gelila-tilahun <gelila.t2020@gmail.com>
2026-07-08 12:01:42 +0300

    Refactor the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,27 +8,38 @@ import { usePathname } from 'next/navigation';
 import { FaBugs } from "react-icons/fa6";
 
 const NavBar = () => {
-  const currentPath = usePathname();
-  const { status, data: session  } = useSession();
 
-  const links = [
-    {label: 'Dashboard',  href: '/'},
-    {label: 'Issues', href: '/issues/list'}
-  ]
   return (
     <nav className='flex space-x-6 border-b-2 mb-5 px-5 h-14 items-center'>  
       <Container>
         <Flex justify="between">
           <Flex align="center" gap="3">
-            <Link href="/"> <FaBugs /></Link> 
-      <ul className='flex space-x-6'>
+            <Link href="/">
+               <FaBugs/>
+            </Link>  
+             <NavLinks />     
+          </Flex> 
+            <AuthStatus/>    
+        </Flex>
+    </Container> 
+    </nav>
+);
+};
+
+const NavLinks = () => {
+    const currentPath = usePathname();
+    const links = [
+      {label: 'Dashboard',  href: '/'},
+      {label: 'Issues', href: '/issues/list'}
+  ];
+
+      return (<ul className='flex space-x-6'>
         {links.map((link) =>(
            <li  key={link.href} >
            <Link             
               className= {classnames({
-                'text-zinc-900': link.href === currentPath,
-                'text-zinc-500': link.href !== currentPath,
-                'hover:text-xinc-800 transition-colors' : true
+                "nav-link": true,
+                '!text-zinc-900': link.href === currentPath,
               })}
               href={link.href}
               > 
@@ -37,12 +48,22 @@ const NavBar = () => {
             </li>
           ))}  
       </ul>
-          </Flex>
-          <Box>
-            { status === "authenticated" && (
+      );
+};
+
+const AuthStatus = () => {
+   const { status, data: session  } = useSession();
+  
+   if (status === "loading") return null;
+
+   if (status === "unauthenticated")       
+        return <Link className= "nav-link" href="/api/auth/signin">Login</Link>;
+        
+   return(  
+        <Box>
               <DropdownMenu.Root>
                 <DropdownMenu.Trigger>
-                  <Avatar src={session.user!.image!} 
+                  <Avatar src={session!.user!.image!} 
                   fallback="?"
                   size = "2"
                   radius="full"
@@ -52,7 +73,7 @@ const NavBar = () => {
                 <DropdownMenu.Content>
                   <DropdownMenu.Label>
                     <Text size="2">
-                    { session.user!.email }
+                    { session!.user!.email }
                     </Text>
                   </DropdownMenu.Label>
                   <DropdownMenu.Item>
@@ -60,16 +81,8 @@ const NavBar = () => {
                   </DropdownMenu.Item>
                 </DropdownMenu.Content>
                 </DropdownMenu.Root>
-          )}
-          { status === "unauthenticated" && (
-            <Link href="/api/auth/signin">Login</Link>
-          )}
           </Box>          
-        </Flex>
-    </Container> 
-    </nav>
-);
+  );
 };
-
 
 export default NavBar
