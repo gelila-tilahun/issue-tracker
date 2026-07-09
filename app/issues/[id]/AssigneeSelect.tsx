731463138885bde579057d7c7eedbd1f99a7651d
gelila-tilahun <gelila.t2020@gmail.com>
2026-07-09 09:52:02 +0300

    Fetch data with React Query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,32 @@
 'use client';
 import { Select } from '@radix-ui/themes'
-import React from 'react'
+import axios from 'axios';
+import { User } from '@prisma/client';
+import React, { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query';
+import { Skeleton } from '@/app/components'
 
 const AssigneeSelect = () => {
+   const {data: users, error, isLoading}= useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res => res.data),
+    staleTime: 60 * 1000, //60s
+    retry: 3
+  });
+
+ if (isLoading) return <Skeleton/>;
+
+ if (error) return null; 
+
   return (
     <Select.Root>
         <Select.Trigger placeholder='Assign...'/>
         <Select.Content>
             <Select.Group>
                 <Select.Label>Suggestions</Select.Label>
-                <Select.Item value = "1"> Mosh Hamedani </Select.Item>
+                 {users?.map(user => (
+                  <Select.Item key={user.id} value="{String{user.id}}" >{user.name}</Select.Item> )
+                  )}          
             </Select.Group>
         </Select.Content>
     </Select.Root>
